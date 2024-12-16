5aacf7817252d7cd91204ac6148f7fc91e939fa5
Tathagata Chakraborty <tathai96@gmail.com>
2024-12-16 11:27:52 +0530

    Added Grid Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import {Button} from "@chakra-ui/react"
-import {RiMailLine} from "react-icons/ri";
+import {Grid, GridItem, Show} from "@chakra-ui/react"
 
 function App() {
   return (
-          <div>
-              <Button colorPalette="teal" variant="solid">
-                  <RiMailLine /> Email
-              </Button>
-          </div>
+          <Grid templateAreas={{
+              base: `"nav" "main"`,
+              lg: `"nav nav" "aside main"`, //wider than 1024 px
+          }}>
+              <GridItem area="nav" bg="coral">Nav</GridItem>
+              <GridItem display={{ base: "none", lg: "block" }} area="aside" bg="gold">Aside</GridItem>
+              <GridItem area="main" bg="dodgerblue">Main</GridItem>
+          </Grid>
   );
 }
 

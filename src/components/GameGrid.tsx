387e498bc9694870c861fbc5e6b861e0d504cc4b
Tathagata Chakraborty <tathai96@gmail.com>
2024-12-19 13:20:32 +0530

    Implemented Infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,8 +1,10 @@
-import {SimpleGrid, Text} from "@chakra-ui/react";
+import {SimpleGrid, Spinner, Text} from "@chakra-ui/react";
 import useGames from "../hooks/useGames.ts";
 import GameCard from "./GameCard.tsx";
 import GameCardSkeleton from "./GameCardSkeleton.tsx";
-import {Genre, PlatformDetails} from "../model.ts";
+import {FetchGamesResponse, Game, Genre, PlatformDetails} from "../model.ts";
+import InfiniteScroll from "react-infinite-scroll-component";
+import {Fragment} from "react";
 
 interface GameGridProps {
     selectedGenre: Genre | null;
@@ -12,17 +14,32 @@ interface GameGridProps {
 }
 
 const GameGrid = ({selectedGenre, selectedPlatform, selectedSortOrder, searchValue}: GameGridProps) => {
-    const {data: games, error, isLoading} = useGames(selectedGenre, selectedPlatform, selectedSortOrder, searchValue);
+    const {data, error, isLoading, fetchNextPage, hasNextPage} = useGames(selectedGenre, selectedPlatform, selectedSortOrder, searchValue);
     const skeletons = [1,2,3,4,5,6];
 
+    if (error) return <Text>{error.message}</Text>;
+
+    const fetchedGamesCount =
+        data?.pages.reduce(
+            (total, page) => total + page.results.length,
+            0
+        ) || 0;
+
     return (
-        <>
-            {error && <Text color="red">{isLoading}</Text>}
+        <InfiniteScroll dataLength={fetchedGamesCount} hasMore={!!hasNextPage} next={() => fetchNextPage()} loader={<Spinner />}>
             <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} padding="10px" gap={3}>
                 {isLoading && skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
-                {games?.results.map(game => <GameCard key={game.id} game={game} />)}
+                {/*{games.pages.map(game => <GameCard key={game.id} game={game} />)}*/}
+                {data?.pages.map((page: FetchGamesResponse, index: number) => (
+                    <Fragment key={index}>
+                        {page.results.map((game: Game) => (
+                            <GameCard key={game.id} game={game} />
+
+                        ))}
+                    </Fragment>
+                ))}
             </SimpleGrid>
-        </>
+        </InfiniteScroll>
     )
 }
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,20 +1,27 @@
 import apiClient from "../services/api-client.ts";
 import {FetchGamesResponse, Genre, PlatformDetails} from "../model.ts";
-import {useQuery} from "@tanstack/react-query";
+import {useInfiniteQuery} from "@tanstack/react-query";
 
-const useGames = (selectedGenre: Genre | null, selectedPlatform: PlatformDetails | null, selectedSortOrder: string | null, searchValue: string | null) => useQuery({
+const useGames = (selectedGenre: Genre | null, selectedPlatform: PlatformDetails | null, selectedSortOrder: string | null, searchValue: string | null) => useInfiniteQuery({
     queryKey: ['games', selectedGenre, selectedPlatform, selectedSortOrder, searchValue],
-    queryFn: () =>
+    queryFn: ({pageParam = 1}: { pageParam?: number }) =>
         apiClient
             .get<FetchGamesResponse>('/games', {
                 params: {
                     genres: selectedGenre?.id,
                     parent_platform: selectedPlatform?.id,
                     ordering: selectedSortOrder,
-                    search: searchValue
+                    search: searchValue,
+                    page: pageParam
                 }
             })
             .then(res => res.data),
+    initialData: undefined,
+    initialPageParam: undefined,
+    getNextPageParam: (lastPage, allPages) => {
+        return lastPage.next ? allPages.length + 1 : undefined;
+    },
+    staleTime: 24*60*60*1000,
 
 })
 // params: {genres: selectedGenre?.id, platforms: selectedPlatform?.id, ordering: selectedSortOrder, search: searchValue}
